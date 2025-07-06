6fbeac9417e6669f85be1ae606ca5b46ba0ffc50
Ayushmaan Dwivedi <ayushmaandwivediiaf@gmail.com>
2025-07-06 13:33:33 +0530

    fetch data from the backend

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -11,6 +11,13 @@ export function HomePage() {
             <div className="home-page">
                 <div className="products-grid">
                     {products.map((product) => {
+                         fetch('http://localhost:3000/api/products')
+                         .then((response)=>{
+                                    return  response.json()
+                         }).then((data)=>{
+                            console.log(data)
+                                        
+                          });
                         return (
                             <div key={product.id} className="product-container">
                                 <div className="product-image-container">
